4b0b9696cf1faaffdb4c76eea91bd5bd3202648a
sleepingAnt <sleeping_ant@qq.com>
2012-11-20 18:10:49 +0800

    编辑课程界面的设计

--- gen/com/ant/timetable/R.java
+++ gen/com/ant/timetable/R.java
@@ -24,20 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int detail_content=0x7f080005;
-        public static final int expandableListView1=0x7f080003;
-        public static final int menu_settings=0x7f08000d;
+        public static final int autoCompleteTextView1=0x7f080003;
+        public static final int detail_content=0x7f080009;
+        public static final int editText1=0x7f080005;
+        public static final int editText2=0x7f080007;
+        public static final int menu_settings=0x7f080010;
         public static final int pager=0x7f080000;
         public static final int pager_titlmViewPagermViewPagere_strip=0x7f080001;
         public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f08000b;
-        public static final int textView4=0x7f08000c;
-        public static final int textView5=0x7f08000a;
-        public static final int tv_classroom_m_1=0x7f080007;
-        public static final int tv_course_m_1=0x7f080006;
-        public static final int tv_course_m_2=0x7f080009;
-        public static final int tv_morning=0x7f080004;
-        public static final int tv_teacher_m_1=0x7f080008;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080006;
+        public static final int textView4=0x7f08000f;
+        public static final int textView5=0x7f08000e;
+        public static final int tv_classroom_m_1=0x7f08000b;
+        public static final int tv_course_m_1=0x7f08000a;
+        public static final int tv_course_m_2=0x7f08000d;
+        public static final int tv_morning=0x7f080008;
+        public static final int tv_teacher_m_1=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
